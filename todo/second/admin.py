a2a4ce8b0930ab1f38ca0dcbc1c682eff5855c95
schw240 <schw240@gmail.com>
2020-09-05 03:39:24 +0900

    CSS 페이지만들기 프로젝트 + 자바스크립트 가위바위보 프로젝트

--- todo/second/admin.py
+++ todo/second/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import FavouriteGroup
+from .models import FavouriteGroup, Favourite, Todo, TodoGroup
 
 # Register your models here.
 @admin.register(FavouriteGroup)
 class FavouriteGroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TodoGroup)
+class TodoGroupAdmin(admin.ModelAdmin):
     pass
--- todo/second/models.py
+++ todo/second/models.py
@@ -5,3 +5,33 @@ class FavouriteGroup(models.Model):
     seq = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     reg_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Favourite(models.Model):
+    seq = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=100)
+    memo = models.TextField()
+    reg_date = models.DateField(auto_now_add=True)
+    group = models.ForeignKey(FavouriteGroup, on_delete=models.CASCADE)
+
+
+class TodoGroup(models.Model):
+    seq = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    reg_date = models.DateField(auto_now_add=True)
+    del_yn = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Todo(models.Model):
+    seq = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    reg_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField()
+    del_yn = models.BooleanField()
+    group = models.ForeignKey(TodoGroup, on_delete=models.CASCADE)
